7a1d88e2e769e5afbc9bc9c54f70d90e0f75c9be
mettil13 <matildemariano01@gmail.com>
2024-10-09 12:23:23 +0200

    Camera manager + fix grid manager

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Camera mainCamera;
+
+    private bool isMouseHeldDown = false;
+    private Vector3 oldPanningPosition = Vector3.zero;
+
+
+    private void Awake() {
+        mainCamera = GetComponentInChildren<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        bool newMouseHeldDown = Input.GetMouseButton(0);
+
+        if (newMouseHeldDown && GridManager.Instance.selectedTile == null) {
+            if(!isMouseHeldDown) {
+                oldPanningPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            Vector3 movement = oldPanningPosition - mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            movement.y = 0;
+            transform.position += movement;
+            oldPanningPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        isMouseHeldDown = newMouseHeldDown;
     }
 }
