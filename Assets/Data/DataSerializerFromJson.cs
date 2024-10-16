a5efcf3e8e437c415745ae768707049cd0b5a735
mettil13 <matildemariano01@gmail.com>
2024-10-16 22:31:41 +0200

    import balance data from json WIP

--- Assets/Data/DataSerializerFromJson.cs
+++ Assets/Data/DataSerializerFromJson.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class DataSerializerFromJson : MonoBehaviour
 {
     public TextAsset textAssetData;
-    public List<Dictionary<string, string>> rawData = new List<Dictionary<string, string>>();
+    public List<PlaceableSO> placeablesSO;
 
     private void Start() {
         DeserializeJSONToSO(textAssetData.text);
@@ -14,12 +14,17 @@ public class DataSerializerFromJson : MonoBehaviour
 
     public void DeserializeJSONToSO(string json) {
         List<string> dataList = FromJSONListToStringList(json);
+        List<Dictionary<string, string>> rawData = new List<Dictionary<string, string>>();
+        List<NamePeaker> dataNames = new List<NamePeaker>();
         foreach(string str in dataList) {
             rawData.Add(JsonUtility.FromJson<Dictionary<string, string>>(str));
+            dataNames.Add(JsonUtility.FromJson<NamePeaker>(str));
         }
 
+        Debug.Log(dataNames[0].name);
+
         StructureSO myScriptableObject = ScriptableObject.CreateInstance<StructureSO>();
-        AssetDatabase.CreateAsset(myScriptableObject, "Assets/NewSO.asset");
+        AssetDatabase.CreateAsset(myScriptableObject, "Assets/NewSO.asset"); // TODO: mark it as dirty
         JsonUtility.FromJsonOverwrite(dataList[0], myScriptableObject);
     }
 
@@ -48,4 +53,8 @@ public class DataSerializerFromJson : MonoBehaviour
         }
         return list;
     }
+}
+[System.Serializable]
+public class NamePeaker {
+    public string name;
 }
