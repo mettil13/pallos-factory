b20a471e778beb77afb02048e0469f23b016fee6
ElanProject <126772054+ElanSSHProject@users.noreply.github.com>
2024-10-29 16:47:53 +0100

    transporter added to shop

--- Assets/Data/DataSerializerFromJson.cs
+++ Assets/Data/DataSerializerFromJson.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -58,3 +59,4 @@ public class DataSerializerFromJson : MonoBehaviour
 public class NamePeaker {
     public string name;
 }
+#endif
